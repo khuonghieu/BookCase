2270c09040680a82c8ba1ff216964354afb8549f
Hieu Khuong <tug60121@temple.edu>
2019-03-31 20:43:05 -0400

    Added Book class.

---
+++ app/src/main/java/edu/temple/bookcase/Book.java
@@ -0,0 +1,60 @@
+package edu.temple.bookcase;
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+    private int published;
+    private String coverURL;
+
+    public Book(int id, String title, String author, int published, String coverURL) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.published = published;
+        this.coverURL = coverURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublished() {
+        return published;
+    }
+
+    public void setPublished(int published) {
+        this.published = published;
+    }
+
+    public String getCoverURL() {
+        return coverURL;
+    }
+
+    public void setCoverURL(String coverURL) {
+        this.coverURL = coverURL;
+    }
+
+
+}
+
